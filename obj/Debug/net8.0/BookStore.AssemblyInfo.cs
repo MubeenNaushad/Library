10b85b5fcf32ca194173fab6318abe12fc3c7ab7
Mubeen <114735488+MubeenNaushad@users.noreply.github.com>
2025-09-28 17:19:45 +0500

    Library

--- obj/Debug/net8.0/BookStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd373a8f92dd122047c743ceeb241dd690177450")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
